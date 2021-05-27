5eec848adaf63438a3b401ad9701e0ce698f2913
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2021-05-26 22:03:59 -0700

    new api

--- bin/jeremy.rb
+++ bin/jeremy.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/picklecraft/client'
 
 def cage(p, pos, type)
+  puts "Building a cage around #{pos} of #{type}"
   x, y, z = pos
   (-2..2).each do |a_offset|
     (-2..2).each do |b_offset|
@@ -15,10 +16,27 @@ end
 
 p = Picklecraft::Client.new(server: 'localhost', verbose: true)
 
-mika = p.find_player name: 'mikalightsmith'
-# jeremy = p.find_player name: 'jeremylightsmith'
+# mika = p.player name: 'mikalightsmith'
+# jeremy = p.player name: 'jeremylightsmith'
+# pos = mika['position'].map(&:floor)
 
-pos = mika['position'].map(&:floor)
+p.on_command do |event|
+  puts "event = #{event.inspect}"
+  case event['event']
+  when 'command_event'
+    case event['command']
+    when '/cage'
+      player = event['player']
+      pos = player['position'].map(&:floor)
+      cage(p, pos, 'GLASS')
+    when '/ping'
+      player = event['player']
+      puts p.nearby_entities(player_name: player['name'], range: 50)
+    end
+  end
+end
+
+p.listen
 
 # cage(p, pos, 'NETHERITE_BLOCK')
 # cage(p, pos, 'DIAMOND_BLOCK')
--- lib/picklecraft/client.rb
+++ lib/picklecraft/client.rb
@@ -5,27 +5,32 @@ module Picklecraft
   class Client
     def initialize(verbose: false, server: 'localhost', port: 3200)
       @verbose = verbose
+      @server = server
+      @port = port
       @client = Faraday.new(
         url: "http://#{server}:#{port}"
       )
+      @threads = []
     end
 
     def players
       parse(get('/players'))
     end
 
-    def find_player(name:)
-      player = parse(get("/players/#{name}"))
-
-      puts "player = #{player}"
+    def player(name:)
+      player = parse(post('/player', name: name))
 
       raise "#{name} isn't on the server, only found #{players.map { |p| p['name'] }}" unless player['name']
 
       player
     end
 
-    def place_block(params)
-      post('/blocks/new', params)
+    def place_block(type:, x:, y:, z:)
+      post('/place_block', type: type, x: x, y: y, z: z)
+    end
+
+    def nearby_entities(player_name:, range:)
+      parse(post('/nearby_entities', player_name: player_name, range: range))
     end
 
     def get(path, params = {})
@@ -41,6 +46,21 @@ module Picklecraft
     def put(path, body = {})
       puts "PUT #{path} : #{body}" if @verbose
       @client.put(path, body.to_json).body
+    end
+
+    def on_command
+      @threads << Thread.new do
+        socket = TCPSocket.new(@server, @port + 1)
+        while (line = socket.gets)
+          yield JSON.parse(line.strip)
+        end
+        socket.close
+      end
+    end
+
+    def listen
+      puts 'Listening for events, type ctrl-c to stop...'
+      @threads.each(&:join)
     end
 
     private
